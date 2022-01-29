593ad35ce68d351aec6cdab6babee7eb9099d825
Dimitrios Barkas <53380520+dimibarkas@users.noreply.github.com>
2022-01-29 17:02:39 +0100

    add jacoco dependency

---
+++ src/test/java/mutation/SortingTest.java
@@ -0,0 +1,18 @@
+package mutation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SortingTest {
+
+    @Test
+    public void sort() {
+
+        int[] arr = {12, 11, 13, 5, 6};
+        int[] exp = {5, 6, 11, 12, 13};
+        Sorting sorting = new Sorting();
+        sorting.sort(arr);
+        assertArrayEquals(exp, arr);
+    }
+}
